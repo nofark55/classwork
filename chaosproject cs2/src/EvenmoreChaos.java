3305ae19fd987253da1356e9ae9c5d8fc80e3ccf
nofark55 <noahafarkas@gmail.com>
2025-09-24 08:06:13 -0400

    Refactor print statements in EvenmoreChaos class
    Updated print statements to clarify output expectations.

--- chaosproject cs2/src/EvenmoreChaos.java
+++ chaosproject cs2/src/EvenmoreChaos.java
@@ -9,16 +9,17 @@ public class EvenmoreChaos {
     public void Makechaos(){
         boolean chaos = true;
         System.out.println("boomboomboom!!!!!");
-        System.out.println(chaos);
+        System.out.println(chaos); //what will this print?
     }
     public EvenmoreChaos() {
         int chaos = 21;
-        System.out.println(this.chaos);
+        System.out.println(this.chaos); //what wil this print?
         for (evenmorechaos = 0; evenmorechaos < 97; chaos++) {
-            this.evenmorechaos += 3;
+            this.evenmorechaos += 3; //what is the result of this?
             System.out.println(evenmorechaos);
             while (evenmorechaos < 42) {
                 Makechaos();
+                System.out.println(chaos); //what will this print?
                 break; // it loogs boom and the other thing so I just break it even though its a while loop.
             }
         }
